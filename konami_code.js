dfa92071290b4725d3f9008f6fa1d55dbbd8217c
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-27 16:35:48 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,21 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+  let i = 0;
+  
+  document.body.addEventListener('keydown', function(event) {
+    const key = parseInt(event.detail || event.which);
+    
+    if (key === code[i]) {
+      i++;
+      
+      if (i === code.length) {
+        alert("Congratulations, nerd!");
+      }
+    } else {
+      i = 0;
+    }
+    }
+  );
 }
